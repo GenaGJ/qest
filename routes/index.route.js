6569a9c784f715eff74f69b18eb0b9ffa8f75264
ValentinLugovov <mac@MacBook-Pro-mac.local>
2023-09-14 16:03:28 +0300

    Release 1: Add routers base

---
+++ routes/index.route.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const cardListApiRouter = require('./api/cardList.api.route');
+const mainApiRouter = require('./api/main.api.route');
+const questionsApiRouter = require('./api/questions.api.route');
+
+const cardListViewRouter = require('./view/cardList.view.route');
+const mainViewRouter = require('./view/main.view.route');
+const questionsViewRouter = require('./view/questions.view.route');
+
+router.use('/', mainViewRouter);
+router.use('/questions', questionsViewRouter);
+router.use('/cardlist', cardListViewRouter);
+
+router.use('/api/', mainApiRouter);
+router.use('/api/questions', questionsApiRouter);
+router.use('/api/cardlist', cardListApiRouter);
+
+module.exports = router;
